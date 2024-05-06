7232ca8e65f3072a8b0bc5249fad0e3ff6631dd6
GaoNeng-wWw <gaonenggg@outlook.com>
2024-05-06 21:50:28 +0800

    feat: delete api protect now

--- server/middleware/token.ts
+++ server/middleware/token.ts
@@ -1,34 +1,38 @@
 import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
 export default defineEventHandler(async (ctx) => {
-  const rawToken = getCookie(ctx, 'token');
-  if (!rawToken) {
-    throw createError({
-      statusCode: 401,
-      message: '未登录',
-    })
-  }
-  const token = rawToken.toLocaleLowerCase().includes('Bearer') ? rawToken.split(' ')[1] : rawToken;
-  if (!token.startsWith('gh')) {
-    try {
-      veirfyJWT(token)
-    } catch (error) {
-      if (error instanceof TokenExpiredError) {
-        throw createError({
-          statusCode: 401,
-          message: 'token已过期',
-        })
-      }
-      throw createError({
-        statusCode: 401,
-        message: '非法token',
-      })
-    }
-  }
-  if (!await verifyJWTAtRedis(token)) {
-    throw createError({
-      statusCode: 401,
-      message: 'token已过期',
-    })
-  }
+  // const rawToken = getCookie(ctx, 'token');
+  // if (!rawToken) {
+  //   throw createError({
+  //     statusCode: 401,
+  //     message: '未登录',
+  //     fatal: true,
+  //   })
+  // }
+  // const token = rawToken.toLocaleLowerCase().includes('Bearer') ? rawToken.split(' ')[1] : rawToken;
+  // if (!token.startsWith('gh')) {
+  //   try {
+  //     veirfyJWT(token)
+  //   } catch (error) {
+  //     if ('expiredAt' in (error as unknown as Error)) {
+  //       throw createError({
+  //         statusCode: 401,
+  //         message: 'token已过期',
+  //         fatal: true,
+  //       })
+  //     }
+  //     throw createError({
+  //       statusCode: 401,
+  //       message: '非法token',
+  //       fatal: true,
+  //     })
+  //   }
+  // }
+  // if (!await verifyJWTAtRedis(token)) {
+  //   throw createError({
+  //     statusCode: 401,
+  //     message: 'token已过期',
+  //     fatal: true,
+  //   })
+  // }
 })
